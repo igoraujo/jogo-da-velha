957adba3402c8eab239087889b355f2d38136ad6
igoraujo <igoraujo93@gmail.com>
2018-01-14 22:43:49 -0200

    Commit inicial jogo-da-velha

---
+++ src/app/jogo-da-velha/jogo-da-velha.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { JogoDaVelhaComponent } from './jogo-da-velha.component';
+import { JogoDaVelhaService } from './shared';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    JogoDaVelhaComponent
+  ],
+  exports: [
+    JogoDaVelhaComponent,
+    //neste caso não irá se trabalhar com rotas e será renderizado diretamente a tag do component do modulo appComponent
+    //é nescessário para que o módulo veja esta tag
+  ],
+  providers: [
+    JogoDaVelhaService
+  ]
+})
+export class JogoDaVelhaModule { }
